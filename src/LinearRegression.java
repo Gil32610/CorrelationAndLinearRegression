b8e98a7f4aa8c3e0411bae36d193e1efb6e3ed9f
GIL32610 <gil.00000032610@unicap.br>
2023-03-07 12:38:01 -0300

    Akaike Info Criteria and sum of residual squared

--- src/LinearRegression.java
+++ src/LinearRegression.java
@@ -5,26 +5,25 @@ public class LinearRegression {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        double[][] table = { { 5, 6 }, { 8, 9 }, { 7, 8 }, { 10, 10 }, { 6, 5 }, { 7, 7 }, { 9, 8 }, { 3, 4 }, { 8, 6 },
-                { 2, 2 } };
+        double[][] table = { { 0.2, 1.5 }, { 2.7, 1.8 }, { 4.5, 3.1 }, { 5.9, 2.6 }, { 7.2, 3.6 }};
 
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table[0].length - 1; j++) {
-                System.out.println("Value of X: ");
-
-            }
-        }
         print2DArray(table);
         separate();
         double r = linearCorrelationCoefficient(table);
         double r2 = r * r;
         double a = calculateA(table);
         double b = calculateB(a, table);
+        double sqr = sumOfResidualSquared(a, b, table);
+        double aic = akaikeInfoCriteria(sqr, table);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.printf("The sum of residual squared is %.3f %n",sqr);
         System.out.printf("The result equation is Y = %.2fX + (%.2f)%n", a, b);
         separate();
-        System.out.printf("The Linear Correlation Coefficient is %.2f%%%n", (r * 100));
-        System.out.printf("The relationship between the two variables explain %.2f%% of the variation in the data",
-                (r2 * 100));
+        System.out.printf("The Linear Correlation Coefficient is %.2f%n", (r));
+        System.out.printf("The relationship between the two variables explain %.2f of the variation in the data%n",
+                (r2));
+        System.out.printf("The akaike criteria is: %.4f" , aic);
 
     }
 
@@ -93,8 +92,29 @@ public class LinearRegression {
         System.out.println("--------\n".repeat(4));
     }
 
-        // public static Double[][] createEmpty2DArray(int row, int column) {
-        //     Double[][] empty = new Double[row][column];
+    public static double sumOfResidualSquared(double a, double b, double[][]table){
+        double sqr = 0;
+        double diff =0;
+        for(int i =0; i<table.length; i++){
+            for(int j = 0; j<table[0].length-1; j++){
+                diff =(table[i][j+1]) - (table[i][j]*a + b);
+                sqr += Math.pow((diff), 2);
+            }
+        }
+    return sqr;
+    } 
 
-        // }
+    public static double akaikeInfoCriteria(double sqr, double[][] table ){
+        double length = table.length;
+        double fHalf = (table.length)*Math.log((sqr/table.length));
+        double sHalf = (2.0*2.0)*(length/(length-2-1));
+        return(fHalf + sHalf);
+    }
+
+    
+
+    // public static Double[][] createEmpty2DArray(int row, int column) {
+    // Double[][] empty = new Double[row][column];
+
+    // }
 }
